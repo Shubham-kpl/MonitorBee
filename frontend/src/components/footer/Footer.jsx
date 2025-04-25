081153a66d6e01fa082697df21e1e6132dcd4515
SHUBHAM KANDPAL <codewithsk27@gmail.com>
2025-04-25 23:59:43 +0530

    gen-2 contributors

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -5,14 +5,24 @@ const Footer = () => {
   return (
     <footer className="footer-section">
       <div className="container">
-        <p>
-          Created by &copy;{" "}
-          <span style={{ fontWeight: "600" }}>Anushri Rawat</span> ,
-          <span style={{ fontWeight: "600" }}>Mohd. Rehan</span> ,
-          <span style={{ fontWeight: "600" }}>Shushant Kohli</span> and{" "}
-          <span style={{ fontWeight: "600" }}>Atul Chaudhary</span>
-        </p>
+        <p style={{ fontWeight: "250" }}>Created by:</p>
+        <div className="first-gen">
+          <span style={{ fontWeight: "100" }}>First Generation : </span>
+          <span style={{ fontWeight: "400" }}>Anushri Rawat | </span>
+          <span style={{ fontWeight: "400" }}>Mohd. Rehan | </span>
+          <span style={{ fontWeight: "400" }}>Shushant Kohli | </span>
+          <span style={{ fontWeight: "400" }}>Atul Chaudhary</span>
+        </div>
+        <div className="sec-gen">
+          <span style={{ fontWeight: "100" }}>Second Generation : </span>
+          <span style={{ fontWeight: "400" }}>Karan Chanyal | </span>
+          <span style={{ fontWeight: "400" }}>Priya Verma | </span>
+          <span style={{ fontWeight: "400" }}>Shubham Kandpal | </span>
+          <span style={{ fontWeight: "400" }}>Utkarsh Martolia</span>
+        </div>
       </div>
+
+      <span className="copyright">&copy;</span>
     </footer>
   );
 };
