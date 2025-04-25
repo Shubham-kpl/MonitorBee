7810db4909d4bad987c72b1e816e6cdd2864189c
SHUBHAM KANDPAL <codewithsk27@gmail.com>
2025-04-26 02:12:26 +0530

    removed "Features" component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Nav from "./components/navigation/Nav";
 import HomeBanner from "./components/homebanner/HomeBanner";
-import Features from "./components/features/Features";
 import Input from "./components/input/Input";
 import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
@@ -12,7 +11,6 @@ function App() {
     <>
       <Nav />
       <HomeBanner />
-      <Features />
       <Input />
       <WhyBees />
       <About />
