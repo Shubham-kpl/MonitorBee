5b72e1fc508288173a279294d36dc7448226f296
SHUBHAM KANDPAL <codewithsk27@gmail.com>
2025-04-26 01:27:29 +0530

    updated App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import Features from "./components/features/Features";
 import Footer from "./components/footer/Footer";
 import HomeBanner from "./components/homebanner/HomeBanner";
 import WhyBees from "./components/whybees/WhyBees";
+import Nav from "./components/navigation/Nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <HomeBanner />
       <Features />
       <WhyBees />
