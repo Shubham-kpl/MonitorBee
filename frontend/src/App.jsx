44252e7668d4ef383bfc9ac6d7fc9ee4e3679265
SHUBHAM KANDPAL <codewithsk27@gmail.com>
2025-04-26 12:46:58 +0530

    tried adding LiveStream and Routing
    first attempt not successful!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Nav from "./components/navigation/Nav";
 import HomeBanner from "./components/banner/HomeBanner";
 import Input from "./components/input-image-video/Input";
+import LiveStream from "./components/LiveStream";
 import AboutUs from "./components/about-us/About";
 import Footer from "./components/footer/Footer";
 import BeeImportance from "./components/bee-importance/WhyBees";
@@ -11,6 +15,14 @@ function App() {
     <>
       {/* <Nav /> */}
       <Input />
+      <Router>
+        <Routes>
+          {" "}
+          <Route path="/" element={<HomeBanner />} />
+          {/* Add a home page route */}
+          <Route path="/upload-vid" element={<LiveStream />} />
+        </Routes>
+      </Router>
       <HomeBanner />
       <BeeImportance />
       <AboutUs />
